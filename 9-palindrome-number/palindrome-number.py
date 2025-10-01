8de70b54205aeb6792d1c54e89d3c9dd047fc0b1
ahyang <90202399+yang-githubb@users.noreply.github.com>
2025-10-01 14:44:38 +0800

    Time: 0 ms (100.00%) | Memory: 12.5 MB (10.88%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        else:
+            y = str(x)
+            return y == y[::-1]
